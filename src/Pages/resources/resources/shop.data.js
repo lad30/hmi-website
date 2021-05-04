8dd11d5d58fcb31a462ca037feb69c20f6cc512b
Akshay Raskar <akshayraskar@tradesocio.com>
2021-05-04 22:39:32 +0530

    adding resources

---
+++ src/Pages/resources/resources/shop.data.js
@@ -0,0 +1,198 @@
+const SHOP_DATA = {
+ hats: {
+    id: 1,
+    title: 'Toys',
+    routeName: 'hats',
+    items: [
+      {
+        id: 1,
+        name: 'Fourseas',
+        imageUrl: 'https://5.imimg.com/data5/BP/VO/MY-10326267/felt-toys-500x500.jpg',
+        price: 25
+      },
+      {
+        id: 2,
+        name: ' Beanie',
+        imageUrl: 'https://i.pinimg.com/originals/63/0d/18/630d18da9a41d71db4ecf94aa0006bc1.jpg',
+        price: 18
+      },
+      {
+        id: 3,
+        name: 'Brown Cowboy',
+        imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/tronc/HSVUP2MNCFCYFLV3XAKGWZDDJM.jpg',
+        price: 35
+      },
+      {
+        id: 4,
+        name: 'Truck',
+        imageUrl: 'https://image.made-in-china.com/202f0j00oLvfJrRtCeky/Wholesale-Wooden-Container-Loader-Car-Toy-Handmade-Toys.jpg',
+        price: 25
+      },
+      {
+        id: 5,
+        name: 'Sneaky',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/71bP5Xr1K9L._SL1500_.jpg',
+        price: 18
+      },
+      
+    ]
+  },
+  sneakers:{
+    id: 2,
+    title: 'Cutlery & Utensils',
+    routeName: 'sneakers',
+    items: [
+      {
+        id: 10,
+        name: 'Canilla',
+        imageUrl: 'https://i.pinimg.com/originals/4e/96/4d/4e964d94f1d14d4e089a44e237435388.jpg',
+        price: 220
+      },
+      {
+        id: 11,
+        name: ' Yeezy',
+        imageUrl: 'https://i.pinimg.com/originals/35/b4/0e/35b40ee48e2d983a07fbdc790d8ee45a.jpg',
+        price: 280
+      },
+      {
+        id: 12,
+        name: 'Vibgyor',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/81nCemTUqhL._AC_SL1500_.jpg',
+        price: 110
+      },
+      {
+        id: 13,
+        name: 'Iris',
+        imageUrl: 'https://i.etsystatic.com/17892252/r/il/1ca3bc/1682463114/il_570xN.1682463114_fd5z.jpg',
+        price: 160
+      },
+      {
+        id: 14,
+        name: 'Blue lagoon',
+        imageUrl: 'https://i.etsystatic.com/9344457/r/il/02a950/1225189377/il_570xN.1225189377_b320.jpg',
+        price: 160
+      },
+  
+    ]
+  },
+ jackets: {
+    id: 3,
+    title: 'Art',
+    routeName: 'jackets',
+    items: [
+      {
+        id: 18,
+        name: 'Red Shearling',
+        imageUrl: 'https://i.pinimg.com/originals/91/c8/54/91c85486ed9f5bbf61e4df78cede7d9b.jpg',
+        price: 125
+      },
+      {
+        id: 19,
+        name: 'Bottle berry',
+        imageUrl: 'https://i.pinimg.com/564x/a4/32/60/a432607c2f58e184b60a907828354fee.jpg',
+        price: 90
+      },
+      {
+        id: 20,
+        name: 'Skyscraper',
+        imageUrl: 'https://i.ytimg.com/vi/HJ1Erxt9KNU/maxresdefault.jpg',
+        price: 90
+      },
+      {
+        id: 21,
+        name: 'Cherry blossom',
+        imageUrl: 'https://imartnepal.com/wp-content/uploads/2014/07/20-Heart-Melting-Handmade-Valentines-Wreaths-1.jpg',
+        price: 165
+      },
+      {
+        id: 22,
+        name: 'Birdys',
+        imageUrl: 'https://www.mommy-labs.com/wp-content/uploads/2012/11/handmade-birds-paper-mache.jpg',
+        price: 185
+      }
+    ]
+  },
+  womens:{
+    id: 4,
+    title: 'Chocolates',
+    routeName: 'womens',
+    items: [
+      {
+        id: 23,
+        name: 'Ferrero Rocher',
+        imageUrl: 'https://cpimg.tistatic.com/05483819/b/4/Handmade-Chocolate-Assorted-Truffles.jpg',
+        price: 25
+      },
+      {
+        id: 24,
+        name: 'Ministrels',
+        imageUrl: 'https://cdn.w600.comps.canstockphoto.com/large-selection-of-luxury-handmade-stock-photos_csp69664637.jpg',
+        price: 20
+      },
+      {
+        id: 25,
+        name: 'Crunchie',
+        imageUrl: 'https://tiimg.tistatic.com/fp/1/006/608/best-price-handmade-chocolate-bouquet-268.jpg',
+        price: 80
+      },
+      {
+        id: 26,
+        name: 'Bounty',
+        imageUrl: 'https://image.shutterstock.com/image-photo/luxury-handmade-chocolate-600w-119112829.jpg',
+        price: 80
+      },
+      {
+        id: 27,
+        name: 'Twix',
+        imageUrl: 'https://cdn.igp.com/f_auto,q_auto,t_prodm/products/p-scrumptious-coconut-chocolate-laddoo-20-pcs--76469-m.jpg',
+        price: 45
+      },
+  
+    ]
+  },
+ mens: {
+    id: 5,
+    title: 'Bags',
+    routeName: 'mens',
+    items: [
+      {
+        id: 30,
+        name: 'Camo White',
+        imageUrl: 'https://i.pinimg.com/originals/25/77/12/257712669cc6892dacff11961d8e40cf.jpg',
+        price: 325
+      },
+      {
+        id: 31,
+        name: 'Floral Red',
+        imageUrl: 'https://cpimg.tistatic.com/06416875/b/4/Designer-Handmade-Bags.jpg',
+        price: 20
+      },
+      {
+        id: 32,
+        name: 'Black current punch',
+        imageUrl: 'https://notjustashop.arts.ac.uk/wp-content/uploads/Handmade-Bags-Marston-Large-Tote-Just-Kim-Prints-1000x1000.jpg',
+        price: 25
+      },
+      {
+        id: 33,
+        name: 'Green tree',
+        imageUrl: 'https://www.upcycledzine.com/wp-content/uploads/2013/12/Peace4YouBags_Pauline2152_00.jpg',
+        price: 25
+      },
+      {
+        id: 34,
+        name: 'Leather brown',
+        imageUrl: 'https://www.madosh.com/image/cache/data/govind_leather/leather_bags/46/G46-1/9-372x496.jpg',
+        price: 40
+      },
+      {
+        id: 35,
+        name: 'Le-blon mix',
+        imageUrl: 'https://i.pinimg.com/originals/04/dd/d8/04ddd8e988bba2614d53842eaed760e9.jpg',
+        price: 25
+      }
+    ]
+  }
+};
+
+export default SHOP_DATA;
